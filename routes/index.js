303ca65b1df5fc90d7402f0dcf918efe5ecf719d
da crands <davecrands@gmail.com>
2018-02-18 21:49:38 -0500

    Added basic d3 chart

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express'),
-    router  = express.Router();
+    router  = express.Router(),
+    Session = require('../models/session');
 
 
 
 // INDEX ROUTE
 router.get('/', function(req, res){
     res.render('landing');
+});
+
+// CHART
+router.get('/chart', function(req, res){
+    Session.find({}, function(err, allSessions){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('chart/index', {sessions: allSessions});
+        }
+    });
 });
 
 
